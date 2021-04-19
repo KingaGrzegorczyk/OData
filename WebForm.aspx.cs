5f5bef718f88ce4a4f882f27c6e06caaf74e1813
KingaGrzegorczyk <78357910+KingaGrzegorczyk@users.noreply.github.com>
2021-04-19 11:41:49 +0200

    Initial commit

---
+++ WebForm.aspx.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.OData.SampleService.Models.TripPin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OData
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ListPeople();
+        }
+
+
+        protected void ListPeople()
+        {
+            var serviceRoot = "https://services.odata.org/V4/TripPinServiceRW/";
+            var context = new DefaultContainer(new Uri(serviceRoot));
+
+            IEnumerable<Person> people = context.People.Execute();
+
+            GridViewPerson.DataSource = people;
+            GridViewPerson.DataBind();
+
+        }
+    }
+}
